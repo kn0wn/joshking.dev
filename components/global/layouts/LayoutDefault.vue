2417d8ec5fa98d5b9b66ed5443860c2b306eb053
kn0wn <admin@joshking.dev>
2020-03-22 18:06:27 +0000

    feat: added a basic introduction page to get up and running

---
+++ components/global/layouts/LayoutDefault.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero is-fullheight has-background-black">
+    <div class="hero-head">
+      <slot name="head"></slot>
+    </div>
+    <div class="hero-body">
+      <slot></slot>
+    </div>
+    <div class="hero-foot">
+      <slot name="footer"></slot>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,17 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        joshking.dev
-      </h1>
-      <h2 class="subtitle">
-        The front-end for my personal website, built using the new vue
-        composition api&#34;
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <introduction />
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Introduction from '~/components/pages/index/Introduction'
 
 export default {
   components: {
-    Logo
+    Introduction
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
