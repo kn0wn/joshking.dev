995edaa4631c6e5229139fba1f67829251373025
Josh <admin@joshking.dev>
2021-02-10 12:22:21 +0000

    fix: added link component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <main class="flex items-center w-screen h-screen border-t-4 border-blue-600">
-    <div class="container px-16 mx-auto lg:px-44">
+    <div class="container px-8 mx-auto md:px-16 lg:px-44">
       <div class="flex items-center">
         <div class="lg:w-1/2">
-          <h1 class="mb-8 font-mono text-6xl text-gray-800">
-            👋 Hello, <br />
-            <span class="text-5xl">My name is Josh King</span>
-          </h1>
+          <h1 class="mb-8 font-mono text-6xl text-gray-800">👋 Hello</h1>
           <p class="mb-8 text-lg leading-6 text-gray-900">
-            Web developer based in London. Specialising in
-            <a target="_blank" href="//nuxtjs.org" class="underlined">
-              Nuxt.js
-            </a>
+            Web developer based in London. Specialising in front-end development
             and serverless technologies. Building online experiences for clients
             such as Google and Tesla. Engineering Lead at
-            <a target="_blank" href="//rcco.uk" class="underlined">RCCO</a>.
+            <Link link="//rcco.uk">RCCO</Link>
           </p>
           <ul class="flex">
             <li
@@ -22,21 +16,7 @@
               :key="`link-${name}`"
               class="mr-6"
             >
-              <a :href="link" target="_blank" class="relative">
-                {{ name }}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  class="link"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </a>
+              <Link :link="link">{{ name }}</Link>
             </li>
           </ul>
         </div>
@@ -46,6 +26,8 @@
 </template>
 
 <script setup>
+import Link from "./components/Link.vue";
+
 import { ref } from "vue";
 
 const links = ref([
@@ -61,25 +43,3 @@ const links = ref([
 
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
-
-<style lang="postcss">
-.link {
-  @apply absolute top-0 right-0 w-3 h-3;
-  transform: translateX(12px) rotate(45deg);
-}
-
-.underlined {
-  @apply transition-all bg-bottom bg-no-repeat px-1 cursor-pointer;
-  background-image: linear-gradient(
-    theme("colors.blue.500"),
-    theme("colors.blue.500")
-  );
-
-  background-size: auto 10%;
-
-  &:hover {
-    background-size: auto 100%;
-    @apply text-white rounded-sm;
-  }
-}
-</style>
---
+++ src/components/Link.vue
@@ -0,0 +1,59 @@
+<template>
+  <a target="_blank" :href="props.link" class="link">
+    <slot />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      class="arrow"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </a>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+// expects props options
+const props = defineProps({
+  link: String,
+});
+</script>
+
+<style lang="postcss">
+.link {
+  @apply relative;
+
+  &:after {
+    content: "";
+    z-index: -1;
+    top: 100%;
+    left: -0.1em;
+    right: -0.1em;
+    width: 25%;
+    bottom: -10%;
+
+    @apply absolute transition-all bg-blue-600;
+  }
+
+  &:hover {
+    &:after {
+      width: 110%;
+    }
+
+    & .arrow {
+      transform: translateX(12px) rotate(45deg) translateY(-6px);
+    }
+  }
+
+  & .arrow {
+    @apply absolute top-0 right-0 w-3 h-3 transition-all delay-150;
+    transform: translateX(12px) rotate(45deg);
+  }
+}
+</style>
