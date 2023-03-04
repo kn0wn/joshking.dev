557531983d5e2b51f1864dd8de500e66b94d69d3
kn0wn <59017897+kn0wn@users.noreply.github.com>
2023-03-04 14:39:48 +0000

    feat: updated design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,23 @@ module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		colors: {
-			grey: '#f6f6f6',
-			red: '#FF0022'
+			grey: '#adadad',
+			red: '#FF0022',
+			blue: {
+				50: '#f2f5f9',
+				100: '#e6eaf4',
+				200: '#bfcbe3',
+				300: '#99acd2',
+				400: '#4d6eb1',
+				500: '#00308f',
+				600: '#002b81',
+				700: '#00246b',
+				800: '#001d56',
+				900: '#001846'
+			}
 		},
 		fontFamily: {
-			mono: ['Poppins', 'monospace']
+			mono: ['Berkeley Mono', 'monospace']
 		},
 		extend: {}
 	},
